7808f47db3b8ad0bb0530cd798377be8fde1660a
Jakub <jakub.bchyla@gmail.com>
2022-10-11 18:47:10 +0200

    preparing to heroku

--- src/main/java/com/example/JavaGuides28may/controller/EmployeeController.java
+++ src/main/java/com/example/JavaGuides28may/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "https://javaguides28may-emp.herokuapp.com/")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
